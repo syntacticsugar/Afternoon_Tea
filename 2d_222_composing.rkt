9ebd9355389201791105955c87f2e42bfb01ab72
RomyRomy :D <hello@romcreative.com>
2013-01-16 17:35:38 -0500

    adds read/open examples

---
+++ 2d_222_composing.rkt
@@ -0,0 +1,5 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 2d_222_composing) (read-case-sensitive #t) (teachpacks ((lib "image.ss" "teachpack" "2htdp") (lib "batch-io.ss" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.ss" "teachpack" "2htdp") (lib "batch-io.ss" "teachpack" "2htdp")))))
+(write-file "turtle-geometry.dat" "Turtle Geometry presents an innovative program of mathematical discovery that demonstrates how the effective use of personal computers can profoundly change the nature of a student's contact with mathematics. ")
+(read-file "turtle-geometry.dat")
